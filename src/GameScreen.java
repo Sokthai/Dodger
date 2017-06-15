c8ac11f5ea214433a4b4b038630f8465cce8df45
Allen <sokthaitang@gmail.com>
2017-06-15 12:34:52 -0400

    update picture

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -16,26 +17,31 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.awt.Image;
+import java.util.Random;
 
 public class GameScreen  extends JPanel{
-	ArrayList<Point> enemy;
+	ArrayList<Point> enemy = new ArrayList<Point>();
 	final BufferedImage img;
 	Point p;
+	long t = 0;
+	Random random = new Random();
+	
 	public GameScreen() throws IOException{
 		super.setLayout(new BorderLayout());
 		String path = "hero.png";
-		int speed = 1;
+		int speed = 1000;
         img = ImageIO.read(new File(path));
 		 p = new Point(0, 0);
 		 
-		 
-		 
+		this.setBackground(Color.BLACK);
+		
 		 
 		 ActionListener actionTask = new ActionListener(){
-				public void actionPerformed(ActionEvent event){				
+				public void actionPerformed(ActionEvent event){						
 					p.x = p.x;
 					p.y = p.y + 1;
 					repaint();
+					t++;
 				}
 			};
 			
@@ -49,6 +55,37 @@ public class GameScreen  extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		
-		g.drawImage(img, p.x, p.y, null);
+		if (t % 5 == 0){
+			for (int i = 0; i < 5; i++){
+				int x = random.nextInt(this.getWidth()) + 100;
+				x = (x > 2200)? 0 : x;
+				g.drawImage(img, x, 0, null);
+				enemy.add(new Point(x, 0));
+				System.out.println(this.getWidth());
+			}
+		}
+		
+		int end = (t % 5 == 0)? enemy.size() - 10 : enemy.size();
+		
+//		for (int i = 0; i < end; i++){
+//			int y = random.nextInt(10);
+//			enemy.set(i, new Point(enemy.get(i).x, y));
+//			g.drawImage(img, enemy.get(i).x, enemy.get(i).y,null);
+//			
+//			
+//		}
+		
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
